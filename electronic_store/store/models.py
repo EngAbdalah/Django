fe2bf744960b5c13eddac9004206ae66097ac1ad
abdulrahman <abdulrahman@abadc.com.sa>
2025-05-14 21:48:44 +0300

    lab1 after add media dir

--- electronic_store/store/models.py
+++ electronic_store/store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='store/products/', blank=True, null=True)
     image_url = models.URLField(blank=True, null=True)
     sku = models.CharField(max_length=50, unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
